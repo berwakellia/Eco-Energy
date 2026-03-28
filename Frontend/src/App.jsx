ef63b2c4c075d38f7a08e23a3027032454f7800c
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-28 13:41:50 +0200

    fix: move CTA arc line below content to prevent overlap

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,7 @@
-import HeroSection from "./components/HeroSection"
-import BuldingBrand from "./components/BuldingBrand";
-import NextSection from "./components/NextSection";
-import BusinessSolutions from "./components/BusinessSolutions";
-import JoinGalaxy from "./components/JoinGalaxy";
-import LaunchCTA from "./components/LaunchCTA";
+
 function App(){
-  return(<>
-    
-    <HeroSection />
-    <BuldingBrand />
-    <NextSection />
-    <BusinessSolutions />
-    <JoinGalaxy />
-    <LaunchCTA />
-    </>
+  return(
+    <h1>hello Berwa</h1>
   )
 }
 export default App;

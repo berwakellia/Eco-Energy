b1fd07c8c30029ec5f3c45f0e90837ee1ecc245e
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-28 13:37:19 +0200

    added routes needed on the header and also the footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,31 @@
-function App(){
-  return(
-    <h1>berwa to</h1>
-  )
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="flex flex-col min-h-screen">
+        
+        {/* Header */}
+        <Header />
+
+        {/* Main Content */}
+        <main className="grow">
+          <Routes>
+            <Route path="/" element={<h1>the home page</h1>} />
+            <Route path="/about" element={<h1>the about page</h1>} />
+            <Route path="/features" element={<h1>the features page</h1>} />
+            <Route path="/how-it-works" element={<h1>the HowItWorks page</h1>} />
+            <Route path="/contact" element={<h1>the contact page</h1>} />
+          </Routes>
+        </main>
+
+        {/* Footer */}
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
+
 export default App;

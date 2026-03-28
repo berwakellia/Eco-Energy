aa444560cb333b62315516a286d7b2f8b68bef67
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-28 13:00:02 +0200

    feat(ui): update BusinessSolutions to dark green theme and fix Vite compatibility

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import HeroSection from "./components/HeroSection"
 import BuldingBrand from "./components/BuldingBrand";
 import NextSection from "./components/NextSection";
+import BusinessSolutions from "./components/BusinessSolutions";
 function App(){
   return(<>
     
     <HeroSection />
     <BuldingBrand />
     <NextSection />
+    <BusinessSolutions />
     </>
   )
 }

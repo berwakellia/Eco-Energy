a735553c4a744577f20547bc274b2e6c44a462d1
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-04-04 00:00:51 +0200

    updated some conflicting routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,24 +10,16 @@ function App() {
     <BrowserRouter>
       <div className="flex flex-col min-h-screen">
         
-        {/* Header */}
         <Header />
-
-        {/* Main Content */}
         <main className="grow">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<h1>the about page</h1>} />
+            <Route path="/about" element={<AboutUs />} />
             <Route path="/marketing" element={<h1>the marketing size page</h1>} />
             <Route path="/how-it-works" element={<HowItWorks />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="/features" element={<h1>the features page</h1>} />
-            <Route path="/how-it-works" element={<h1>the HowItWorks page</h1>} />
             <Route path="/contact" element={<h1>the contact page</h1>} />
           </Routes>
         </main>
-
-        {/* Footer */}
         <Footer />
       </div>
     </BrowserRouter>
